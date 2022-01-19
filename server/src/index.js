65bd5eee1ccfeb19a16e52bccb1771c0aef71191
jabbcode <jabbsontouch@gmail.com>
2022-01-18 23:03:03 -0400

    creando y configurando servidor con express, cors y variables de entorno

---
+++ server/src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const cors = require('cors');
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hola mundo')
+})
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${ process.env.PORT }`);
+})
